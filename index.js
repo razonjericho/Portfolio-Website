475633709c0970b40901fa8e1e0f68e85d6bdf03
Jericho Pete Razon <razonjerichopete@gmail.com>
2026-08-11 02:46:34 +0800

    remove index.js file

--- index.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector('.hamburger');
-const navUL = document.querySelector('nav ul');
-
-button.addEventListener("click", function() {
-    navUL.classList.toggle(`nav-active`);
-    button.classList.toggle(`nav-active`);
-});
